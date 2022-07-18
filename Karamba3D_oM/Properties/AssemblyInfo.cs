99c638588a4719aa837a0a9c8904d631db8dfd9f
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:08 +0100

    Update Assembly Information

--- Karamba3D_oM/Properties/AssemblyInfo.cs
+++ Karamba3D_oM/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Filing_oM")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/Karamba3D_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Filing_oM")]
@@ -55,6 +55,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.1.0.0")]
+[assembly: AssemblyFileVersion("5.3.0.0")]
 
 
