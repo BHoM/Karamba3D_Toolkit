735cb57d984b7d1047ba55ffe68a5a457606f953
EnricoAntolini <66356545+EnricoAntolini@users.noreply.github.com>
2022-12-02 12:43:17 +0100

    KarambaCommon NuGet added to tests

--- Karamba3D_ToolkitTests/LoadCaseTests.cs
+++ Karamba3D_ToolkitTests/LoadCaseTests.cs
@@ -13,36 +13,37 @@
         [Test]
         public void LoadCases_WhenMultiple_AreStoredAsExpected_Test()
         {
-            // Arrange
-            var loads = new Load[]
-            {
-                new PointLoad(1, new Vector3(1, 2, 3), new Vector3(4, 5, 6), "LoadCase1", false),
-                new ConcentratedForce(
-                    new[] { string.Empty },
-                    null,
-                    "LoadCase2",
-                    0.6,
-                    new Vector3(7, 8, 9),
-                    LoadOrientation.global),
-                new ConcentratedMoment(
-                    new[] { string.Empty },
-                    null,
-                    "LoadCase1",
-                    0.6,
-                    new Vector3(10, 11, 12),
-                    LoadOrientation.global)
-            };
-            var model = TestUtilities.CreateFixedFreeBeam(loads);
+            //// Arrange
+            //var loads = new Load[]
+            //{
+            //    new PointLoad(1, new Vector3(1, 2, 3), new Vector3(4, 5, 6), "LoadCase1", false),
+            //    new ConcentratedForce(
+            //        new[] { string.Empty },
+            //        null,
+            //        "LoadCase2",
+            //        0.6,
+            //        new Vector3(7, 8, 9),
+            //        LoadOrientation.global),
+            //    new ConcentratedMoment(
+            //        new[] { string.Empty },
+            //        null,
+            //        "LoadCase1",
+            //        0.6,
+            //        new Vector3(10, 11, 12),
+            //        LoadOrientation.global)
+            //};
+            //var model = TestUtilities.CreateFixedFreeBeam(loads);
 
-            // Act
-            var bhomModel = model.ToBhOM();
-            var loadCases = bhomModel.LoadCases;
+            //// Act
+            //var bhomModel = model.ToBhOM();
+            //var loadCases = bhomModel.LoadCases;
 
-            // Assert
-            var expectedNames = new[] { "LoadCase1", "LoadCase2" };
-            var expectedNumbers = new[] { 0, 1 };
-            CollectionAssert.AreEqual(expectedNames, loadCases.Select(l => l.Name).ToArray());
-            CollectionAssert.AreEqual(expectedNumbers, loadCases.Select(l => l.Number).ToArray());
+            //// Assert
+            //var expectedNames = new[] { "LoadCase1", "LoadCase2" };
+            //var expectedNumbers = new[] { 0, 1 };
+            //CollectionAssert.AreEqual(expectedNames, loadCases.Select(l => l.Name).ToArray());
+            //CollectionAssert.AreEqual(expectedNumbers, loadCases.Select(l => l.Number).ToArray());
+            Assert.Fail();
         }
     }
 }
